7be2da0fd1ef63a49e3ed644052d863db424b205
rhogger <rhoggerrv@gmail.com>
2023-01-21 18:53:19 -0300

    :heavy_check_mark: Projeto Finalizado

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  study: ['01-16', '01-17', '01-18', '01-19', '01-20', '01-21', '01-22', '01-23', '01-24', '01-25', '01-26', '01-27', '01-28', '01-29', '01-30', '01-31'],
-  water: [],
-  exercise: [],
-  healthy: [],
-  sleep: ['01-17', '01-18'],
-  dog: []
+button.addEventListener('click',add)
+
+function add(){
+  const today = new Date().toLocaleDateString('pt-br').slice(0,5)
+  // const today = '20/01'
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists){
+    alert('Dia já está incluso!')
+    return
+  }
+  
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+form.addEventListener('change', save)
 
+function save(){
+  localStorage.setItem('NLWSetup@Habits',JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@Habits')) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load() 
